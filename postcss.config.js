b89bf7342ac177f3807109f52d62a19a63de84e8
Sagar Desai <sagar@sagars-mbp.lan>
2024-05-30 17:19:58 -0500

    ignore linting

--- postcss.config.js
+++ postcss.config.js
@@ -3,9 +3,4 @@ module.exports = {
     tailwindcss: {},
     autoprefixer: {},
   },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
 };
