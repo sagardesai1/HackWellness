bd235cb2b6acd6521f432b4d64612b6b4fd5cdaf
Sagar Desai <sagar@sagars-mbp.mynetworksettings.com>
2024-06-05 18:05:17 -0400

    adding pricing

---
+++ app/AndrewHuberman/page.tsx
@@ -0,0 +1,192 @@
+import Image from "next/image";
+import React from "react";
+import AndrewHubermanAvatar from "@/images/AuthorImages/AndrewHuberman.jpg";
+import Link from "next/link";
+import { Badge } from "@/components/ui/badge";
+import {
+  Wrench,
+  Footprints,
+  Microscope,
+  Highlighter,
+  EarthLock,
+  ListTodo,
+  Telescope,
+} from "lucide-react";
+
+function page() {
+  return (
+    <div className="flex flex-col mt-6">
+      <div className="flex flex-col p-4 items-center justify-between mx-auto max-w-2xl text-center">
+        <Image
+          src={AndrewHubermanAvatar}
+          alt={"User name"}
+          width={150}
+          height={150}
+          className="rounded-full"
+        />
+        <p className="text-2xl font-semibold">Andrew Huberman</p>
+        <p className="text-gray-500 text-sm mt-1 dark:text-gray-400">
+          Andrew Huberman, Ph.D., is a neuroscientist and tenured professor in
+          the Department of Neurobiology and by courtesy, Psychiatry and
+          Behavioral Sciences at Stanford School of Medicine.
+        </p>
+      </div>
+      <div className="flex flex-col items-center justify-between mt-2">
+        <p className="text-3xl font-bold">Quickly Improve Focus</p>
+        <Badge
+          className="bg-red-100 text-red-600 dark:bg-red-900 dark:text-white mt-2"
+          variant="outline"
+        >
+          Mental Health
+        </Badge>
+      </div>
+      <div className="px-10 py-8">
+        <div className="flex flex-col p-10 mt-6 shadow-lg rounded-xl">
+          <div className="flex items-center gap-2">
+            <div className="bg-indigo-100 rounded-2xl p-1">
+              <ListTodo className="text-indigo-600 h-5 w-5" />
+            </div>
+            <p className="text-2xl font-semibold">Habits</p>
+          </div>
+          <ul className="mt-1 list-disc px-8 text-sm">
+            <li>
+              Practice focusing your visual system at the precise distance from
+              the work you intend to do
+            </li>
+            <li>
+              Eliminate distractions, such as turning off Wi-Fi and putting your
+              phone in another room
+            </li>
+            <li>
+              Maintain visual focus on the task at hand, even if your attention
+              drifts
+            </li>
+            <li>
+              Use blinking strategically to reset your perception of time and
+              space
+            </li>
+            <li>
+              Devote your neurochemical resources to active learning experiences
+              rather than passive ones
+            </li>
+          </ul>
+          <div className="flex items-center gap-2 mt-4">
+            <div className="bg-indigo-100 rounded-2xl p-1">
+              <Telescope className="text-indigo-600 h-5 w-5" />
+            </div>
+            <p className="text-2xl font-semibold">Key Insights</p>
+          </div>
+          <ul className="mt-1 list-disc px-8 text-sm">
+            <li>
+              Visual focus plays a crucial role in mental focus and attention.
+              By training your visual system to focus on a specific target, you
+              can improve cognitive abilities and increase depth and duration of
+              focus.
+            </li>
+            <li>
+              Timing is important for optimizing learning. Understanding your
+              own alertness patterns and scheduling learning sessions during
+              peak alertness can enhance focus and cognitive performance.
+            </li>
+            <li>
+              Minimizing distractions, especially from devices like phones, is
+              essential for improving focus. Creating a dedicated study
+              environment and removing potential distractions can significantly
+              enhance mental focus.
+            </li>
+            <li>
+              Rest and deliberate disengagement are crucial for consolidating
+              learning and promoting plasticity. Taking breaks, engaging in
+              physical activities, or even mindlessly resting can enhance the
+              brain’s ability to retain information and improve cognitive
+              abilities.
+            </li>
+            <li>
+              It’s important to find the right balance when it comes to
+              alertness-enhancing substances like caffeine or medications for
+              ADHD. Working with a qualified professional can help determine the
+              appropriate use and potential risks associated with such
+              substances.
+            </li>
+            <li>
+              Focusing on meaningful and enriching activities rather than
+              passive experiences can lead to greater personal growth and
+              cognitive development. Allocating attention to tasks that align
+              with your goals and values can result in more fulfilling outcomes.
+            </li>
+          </ul>
+          <div className="flex items-center gap-2 mt-4">
+            <div className="bg-indigo-100 rounded-2xl p-1">
+              <Highlighter className="text-indigo-600 h-5 w-5" />
+            </div>
+            <p className="text-2xl font-semibold">Highlights</p>
+          </div>
+          <ul className="mt-1 list-disc px-8 text-sm">
+            <li>Visual focus is closely tied to mental focus and attention.</li>
+            <li>
+              Alertness can be achieved through various means, including
+              caffeine and behavioral practices.
+            </li>
+            <li>
+              Focusing your visual attention on a specific target can enhance
+              mental focus and increase acetylcholine and epinephrine release.
+            </li>
+            <li>
+              Limit distractions from phones and other devices to improve focus.
+            </li>
+            <li>
+              Timing your learning sessions and incorporating rest periods can
+              optimize plasticity and learning.
+            </li>
+            <li>
+              Engaging in deliberate disengagement activities, such as walking
+              or resting, after learning can enhance plasticity.
+            </li>
+          </ul>
+          <div className="flex items-center gap-2 mt-4">
+            <div className="bg-indigo-100 rounded-2xl p-1">
+              <Microscope className="text-indigo-600 h-5 w-5" />
+            </div>
+            <p className="text-2xl font-semibold">Scientific Rationale</p>
+          </div>
+          <ul className="mt-1 list-disc px-8 text-sm overflow-hidden underline">
+            <Link
+              href={"https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6165228/"}
+            >
+              <li>https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6165228/</li>
+            </Link>
+            <Link href={"https://pubmed.ncbi.nlm.nih.gov/27269720/"}>
+              <li>https://pubmed.ncbi.nlm.nih.gov/27269720/</li>
+            </Link>
+            <Link
+              href={"https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6430176/"}
+            >
+              <li>https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6430176/</li>
+            </Link>
+          </ul>
+          <div className="flex items-center gap-2 mt-4">
+            <div className="bg-indigo-100 rounded-2xl p-1">
+              <EarthLock className="text-indigo-600 h-5 w-5" />
+            </div>
+            <p className="text-2xl font-semibold">Sources</p>
+          </div>
+          <ul className="mt-1 list-disc px-8 text-sm overflow-hidden underline">
+            <Link href={"https://www.hubermanlab.com/"}>
+              <li>https://www.hubermanlab.com/</li>
+            </Link>
+          </ul>
+        </div>
+        <div className="mt-10 flex items-center justify-center gap-x-6">
+          <Link
+            href="/"
+            className="rounded-md bg-indigo-600 px-6 py-3 text-sm font-semibold text-white dark:text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Learn more about Hack Wellness
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default page;
---
+++ app/BryanJohnson/page.tsx
@@ -0,0 +1,190 @@
+import Image from "next/image";
+import React from "react";
+import BryanJohnsonAvatar from "@/images/AuthorImages/BryanJohnson.jpeg";
+import Link from "next/link";
+import { Badge } from "@/components/ui/badge";
+import {
+  Wrench,
+  Footprints,
+  Microscope,
+  Highlighter,
+  EarthLock,
+  ListTodo,
+} from "lucide-react";
+
+function page() {
+  return (
+    <div className="flex flex-col mt-6">
+      <div className="flex flex-col p-4 items-center justify-between mx-auto max-w-2xl text-center">
+        <Image
+          src={BryanJohnsonAvatar}
+          alt={"User name"}
+          width={150}
+          height={150}
+          className="rounded-full"
+        />
+        <p className="text-2xl font-semibold">Bryan Johnson</p>
+        <p className="text-gray-500 text-sm mt-1 dark:text-gray-400">
+          Bryan Johnson is an American entrepreneur, venture capitalist, writer
+          and author. He is the founder and CEO of Kernel, a company creating
+          devices that monitor and record brain activity, and OS Fund, a venture
+          capital firm that invests in early-stage science and technology
+          companies.
+        </p>
+      </div>
+      <div className="flex flex-col items-center justify-between mt-2">
+        <p className="text-3xl font-bold">Oral Care Routine 2024</p>
+        <Badge
+          className="bg-orange-100 text-orange-600 dark:bg-orange-900 dark:text-white mt-2"
+          variant="outline"
+        >
+          Oral care
+        </Badge>
+      </div>
+      <div className="px-10 py-8">
+        <div className="flex flex-col p-10 mt-6 shadow-lg rounded-xl">
+          <div className="flex items-center gap-2">
+            <div className="bg-indigo-100 rounded-2xl p-1">
+              <Wrench className="text-indigo-600 h-5 w-5" />
+            </div>
+            <p className="text-2xl font-semibold">Tools</p>
+          </div>
+          <ul className="mt-1 list-disc px-8 text-sm underline">
+            <Link href={"https://amzn.to/3Ky8E5q"}>
+              <li>Water flosser</li>
+            </Link>
+            <Link href={"https://amzn.to/4aScRMf"}>
+              <li>Oral-B Toothbrush</li>
+            </Link>
+            <Link href={"https://amzn.to/3yQWQsE"}>
+              <li>Soft toothbrush heads</li>
+            </Link>
+            <Link href={"https://amzn.to/3VtyGgs"}>
+              <li>Mouthwash</li>
+            </Link>
+            <Link href={"https://amzn.to/4eo1rmv"}>
+              <li>Sensodyne Toothpaste</li>
+            </Link>
+            <Link href={"https://amzn.to/3VtyGgs"}>
+              <li>Dr. Collins Biomin Tooth Sensitivity Toothpaste</li>
+            </Link>
+            <Link href={"https://amzn.to/4bOeWtL"}>
+              <li>DrTungs Floss</li>
+            </Link>
+            <Link href={"https://amzn.to/3Ksewxg"}>
+              <li>DrTungs Tongue Scraper</li>
+            </Link>
+            <Link href={"https://somnomed.com"}>
+              <li>SomnoDent Bruxism Device</li>
+            </Link>
+          </ul>
+          <div className="flex items-center gap-2 mt-4">
+            <div className="bg-indigo-100 rounded-2xl p-1">
+              <Footprints className="text-indigo-600 h-5 w-5" />
+            </div>
+            <p className="text-2xl font-semibold">Steps</p>
+          </div>
+          <ul className="mt-1 list-decimal px-8 text-sm">
+            <li>Use a water pick to clean your mouth effectively </li>
+            <li>Floss using a suitable flossing product</li>
+            <li>Use an electric toothbrush for thorough cleaning</li>
+            <li>Use a tongue scraper to clean your tongue </li>
+            <li>Consider using mouthwash for fresh breath </li>
+            <li>Address bruxism (teeth grinding) with a suitable device </li>
+            <li>Regularly visit the dentist for check-ups and cleanings</li>
+          </ul>
+          <div className="flex items-center gap-2 mt-4">
+            <div className="bg-indigo-100 rounded-2xl p-1">
+              <ListTodo className="text-indigo-600 h-5 w-5" />
+            </div>
+            <p className="text-2xl font-semibold">Simple habits</p>
+          </div>
+          <ul className="mt-1 list-disc px-8 text-sm">
+            <li>Avoid consuming excessive sugar</li>
+            <li>Be mindful of acidic liquids that can harm teeth </li>
+            <li>Check for dry mouth condition and consult your dentist</li>
+          </ul>
+          <div className="flex items-center gap-2 mt-4">
+            <div className="bg-indigo-100 rounded-2xl p-1">
+              <Highlighter className="text-indigo-600 h-5 w-5" />
+            </div>
+            <p className="text-2xl font-semibold">Highlights</p>
+          </div>
+          <ul className="mt-1 list-disc px-8 text-sm">
+            <li>
+              Water picking, flossing, and using an electric toothbrush are key
+              components of a daily oral care routine. These practices help
+              remove plaque and debris, ensuring a cleaner mouth and healthier
+              teeth.
+            </li>
+            <li>
+              Tongue scraping is often overlooked but plays a significant role
+              in maintaining oral hygiene. It helps remove bacteria and debris
+              from the tongue, improving overall oral health.
+            </li>
+            <li>
+              Bruxism can cause significant damage to teeth and oral health.
+              Finding a suitable mouth guard, such as the Sno Dent device, can
+              help prevent further decay and protect the teeth from wear and
+              tear.
+            </li>
+            <li>
+              Regular dental visits are essential for professional teeth
+              cleaning, stain removal, and monitoring overall oral health. They
+              provide a comprehensive assessment and help detect any potential
+              issues early on.
+            </li>
+            <li>
+              Avoiding sugar and being mindful of acidic liquids can greatly
+              reduce the risk of tooth decay and sensitivity. Dry mouth
+              condition should also be addressed, as saliva plays a crucial role
+              in maintaining a healthy oral environment.
+            </li>
+          </ul>
+          <div className="flex items-center gap-2 mt-4">
+            <div className="bg-indigo-100 rounded-2xl p-1">
+              <Microscope className="text-indigo-600 h-5 w-5" />
+            </div>
+            <p className="text-2xl font-semibold">Scientific Rationale</p>
+          </div>
+          <ul className="mt-1 list-disc px-8 text-sm overflow-hidden underline">
+            <Link href={"https://pubmed.ncbi.nlm.nih.gov/22161438/"}>
+              <li>https://pubmed.ncbi.nlm.nih.gov/22161438/</li>
+            </Link>
+            <Link
+              href={"https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6242348/"}
+            >
+              <li>https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6242348/</li>
+            </Link>
+            <Link
+              href={"https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8751028/"}
+            >
+              <li>https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8751028/</li>
+            </Link>
+          </ul>
+          <div className="flex items-center gap-2 mt-4">
+            <div className="bg-indigo-100 rounded-2xl p-1">
+              <EarthLock className="text-indigo-600 h-5 w-5" />
+            </div>
+            <p className="text-2xl font-semibold">Sources</p>
+          </div>
+          <ul className="mt-1 list-disc px-8 text-sm overflow-hidden underline">
+            <Link href={"https://protocol.bryanjohnson.com/#oral-health/"}>
+              <li>https://protocol.bryanjohnson.com/#oral-health/</li>
+            </Link>
+          </ul>
+        </div>
+        <div className="mt-10 flex items-center justify-center gap-x-6">
+          <Link
+            href="/"
+            className="rounded-md bg-indigo-600 px-6 py-3 text-sm font-semibold text-white dark:text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Learn more about Hack Wellness
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default page;
