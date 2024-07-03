4bda23bb66687e9b1f7182208ba4ec85029292cc
Sagar Desai <mr.sagar576@gmail.com>
2024-07-02 20:51:12 -0400

    newsletter fix

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -6,19 +6,30 @@ import { authors } from "@/app/authors";
 const BASE_URL = "https://hack-wellness.vercel.app";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const urls = authors.flatMap((author) => {
-    const directoryPath = path.join(process.cwd(), "app", author);
-    return getArticlesFromAuthors(directoryPath);
-  });
+  // Create an array of author URLs
+  const authorUrls = authors.map((author) => ({
+    url: `${BASE_URL}/${author}`,
+    lastModified: new Date(),
+  }));
 
+  // Get article URLs from authors
+  const articleUrls = authors
+    .flatMap((author) => {
+      const directoryPath = path.join(process.cwd(), "app", author);
+      return getArticlesFromAuthors(directoryPath);
+    })
+    .map((url) => ({
+      url,
+      lastModified: new Date(),
+    }));
+
+  // Combine base URL, author URLs, and article URLs
   return [
     {
       url: BASE_URL,
       lastModified: new Date(),
     },
-    ...urls.map((url) => ({
-      url,
-      lastModified: new Date(),
-    })),
+    ...authorUrls,
+    ...articleUrls,
   ];
 }
