5c41480c054d0f6fc373f01f903c05d6b61060e2
Sagar Desai <mr.sagar576@gmail.com>
2024-07-03 10:33:54 -0400

    newsletter fix

--- app/robots.ts
+++ app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
-      allow: ["/"].concat(authors),
+      allow: ["/"].concat(authors.map((author) => `/${author}`)),
       disallow: [],
     },
     sitemap: "https://hack-wellness.vercel.app/sitemap.xml",
